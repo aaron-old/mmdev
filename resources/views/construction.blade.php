19d73eb6e7597b30f262a9640f620e273b7f2b23
Aaron Young <mustarddevelopment@gmail.com>
2016-07-18 20:48:43 -0700

    Under construction going up.

---
+++ resources/views/construction.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mustardman Developments</title>
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    </head>
+    <body>
+        <h1>website under construction</h1>
+    </body>
+</html>
