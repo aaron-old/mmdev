f1383dd1dfca7186d2a3d9ed3b5dcdf73a7195ea
Aaron Young <mustarddevelopment@gmail.com>
2016-09-16 14:39:03 -0700

    Getting started.. Some type of nonsense at the airport disconnected my HD at the airport. Lost some setup data. and possibly corrupted the node modules.

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,10 +1,16 @@
 
-
 MMDEV = MMDEV || {} ;
 
 MMDEV.Main = (function () {
 
     return {
 
+        init: function () {
+
+        }
     }
 })();
+
+$(document).ready(function () {
+   alert('jquery');
+});
