43fe790e9d685dc540d1658caaee94c830fd1fc9
Aaron Young <mustarddevelopment@gmail.com>
2016-09-16 15:23:33 -0700

    Adding these file for the start to ensure I have jquery accessible to the application and am able to hit a module.

---
+++ public/js/main-production.js
@@ -0,0 +1,17 @@
+
+MMDEV = MMDEV || {} ;
+
+MMDEV.Main = (function () {
+
+    return {
+
+        init: function () {
+
+        }
+    }
+})();
+
+$(document).ready(function () {
+   alert('jquery');
+});
+//# sourceMappingURL=main-production.js.map
